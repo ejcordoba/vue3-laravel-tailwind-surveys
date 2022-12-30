1b83a77f7eae31cb0d3528e299790273217dd40a
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2022-12-30 14:19:00 +0100

    set Laravel Survey CRUD

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Survey extends Model
 {
-    use HasFactory;
+    // using sluggable to transform title into a slug
+    use HasFactory, HasSlug;
+    protected $fillable = ['user_id', 'title', 'slug', 'status', 'description', 'expire_data'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
