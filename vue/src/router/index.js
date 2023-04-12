4b21bc63d12e1a7e39b3c7972cb4ed9347316355
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2023-04-12 13:25:31 +0200

    Recatching project

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -9,66 +9,66 @@ import Surveys from "../views/Surveys.vue";
 import SurveyView from "../views/SurveyView.vue";
 
 const routes = [
-    {
-        path: "/",
-        redirect: "/dashboard",
+  {
+    path: "/",
+    redirect: "/dashboard",
+    name: "Dashboard",
+    component: DefaultLayout,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: "/dashboard",
         name: "Dashboard",
-        component: DefaultLayout,
-        meta: { requiresAuth: true },
-        children: [
-            {
-                path: "/dashboard",
-                name: "Dashboard",
-                component: Dashboard,
-            },
-            {
-                path: "/surveys",
-                name: "Surveys",
-                component: Surveys,
-            },
-            {
-                path: "/surveys/create",
-                name: "SurveyCreate",
-                component: SurveyView,
-            },
-            {
-                path: "/surveys/:id",
-                name: "SurveyView",
-                component: SurveyView,
-            },
-        ],
-    },
-    {
-        path: "/auth",
-        redirect: "/login",
-        name: "Auth",
-        component: AuthLayout,
-        meta: { isGuest: true },
-        children: [
-            {
-                path: "/login",
-                name: "Login",
-                component: Login,
-            },
-            {
-                path: "/register",
-                name: "Register",
-                component: Register,
-            },
-        ],
-    },
+        component: Dashboard,
+      },
+      {
+        path: "/surveys",
+        name: "Surveys",
+        component: Surveys,
+      },
+      {
+        path: "/surveys/create",
+        name: "SurveyCreate",
+        component: SurveyView,
+      },
+      {
+        path: "/surveys/:id",
+        name: "SurveyView",
+        component: SurveyView,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    redirect: "/login",
+    name: "Auth",
+    component: AuthLayout,
+    meta: { isGuest: true },
+    children: [
+      {
+        path: "/login",
+        name: "Login",
+        component: Login,
+      },
+      {
+        path: "/register",
+        name: "Register",
+        component: Register,
+      },
+    ],
+  },
 ];
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 router.beforeEach((to, from, next) => {
-    if (to.meta.requiresAuth && !store.state.user.token) {
-        next({ name: "Login" });
-    } else if (store.state.user.token && to.meta.isGuest) {
-        next({ name: "Dashboard" });
-    } else {
-        next();
-    }
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: "Login" });
+  } else if (store.state.user.token && to.meta.isGuest) {
+    next({ name: "Dashboard" });
+  } else {
+    next();
+  }
 });
 export default router;
